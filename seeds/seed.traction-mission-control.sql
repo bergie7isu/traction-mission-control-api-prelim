be3fe9a810f18025859e64052565323ccdbc1bde
Mark Bergstrom <markbergstrom@gmail.com>
2020-02-07 23:35:14 -0600

    updated seed file to include dashes in to-dos

--- seeds/seed.traction-mission-control.sql
+++ seeds/seed.traction-mission-control.sql
@@ -16,17 +16,17 @@ VALUES
 
 INSERT INTO todos (todo, who, created, due, status, status_date, reviewed, issue)
 VALUES
-  ('First todo', 'Mark Bergstrom', '2019-01-03', '2019-01-10', null, null, 'no', 1),
-  ('Second todo', 'Paul Johnson', '2019-02-03', '2019-02-10', null, null, 'no', 2),
-  ('Third todo', 'Joel Tjepkes', '2019-03-03', '2019-03-10', null, null, 'no', 3),
-  ('Fourth todo', 'Steve Thor', '2019-04-03', '2019-04-10', null, null, 'no', 4),
-  ('Fifth todo', 'Nathan Witt', '2019-05-03', '2019-05-10', null, null, 'no', 5),
-  ('Sixth todo', 'Mark Bergstrom', '2019-06-03', '2019-06-10', 'Done', '2019-06-10', 'yes', 6),
-  ('Seventh todo', 'Paul Johnson', '2019-07-03', '2019-07-10', 'Not Done', '2019-06-10', 'yes', 7),
-  ('Eighth todo', 'Joel Tjepkes', '2019-08-03', '2019-08-10', 'Done', '2019-06-10', 'yes', 8),
-  ('Ninth todo', 'Steve Thor', '2019-09-03', '2019-09-10', 'Not Done', '2019-06-10', 'yes', 9),
-  ('Tenth todo', 'Nathan Witt', '2019-10-03', '2019-10-10', 'Done', '2019-06-10', 'yes', 10),
-  ('Eleventh todo', 'Mark Bergstrom', '2019-11-03', '2019-11-10', 'Not Done', '2019-06-10', 'yes', 11);
+  ('First to-do', 'Mark Bergstrom', '2019-01-03', '2019-01-10', null, null, 'no', 1),
+  ('Second to-do', 'Paul Johnson', '2019-02-03', '2019-02-10', null, null, 'no', 2),
+  ('Third to-do', 'Joel Tjepkes', '2019-03-03', '2019-03-10', null, null, 'no', 3),
+  ('Fourth to-do', 'Steve Thor', '2019-04-03', '2019-04-10', null, null, 'no', 4),
+  ('Fifth to-do', 'Nathan Witt', '2019-05-03', '2019-05-10', null, null, 'no', 5),
+  ('Sixth to-do', 'Mark Bergstrom', '2019-06-03', '2019-06-10', 'Done', '2019-06-10', 'yes', 6),
+  ('Seventh to-do', 'Paul Johnson', '2019-07-03', '2019-07-10', 'Not Done', '2019-06-10', 'yes', 7),
+  ('Eighth to-do', 'Joel Tjepkes', '2019-08-03', '2019-08-10', 'Done', '2019-06-10', 'yes', 8),
+  ('Ninth to-do', 'Steve Thor', '2019-09-03', '2019-09-10', 'Not Done', '2019-06-10', 'yes', 9),
+  ('Tenth to-do', 'Nathan Witt', '2019-10-03', '2019-10-10', 'Done', '2019-06-10', 'yes', 10),
+  ('Eleventh to-do', 'Mark Bergstrom', '2019-11-03', '2019-11-10', 'Not Done', '2019-06-10', 'yes', 11);
 
 INSERT INTO team (name)
 VALUES
